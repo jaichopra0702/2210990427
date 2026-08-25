6986d2878d16bb2213031914642ffa4961ff38dc
jaichopra0702 <122783391+jaichopra0702@users.noreply.github.com>
2026-08-25 14:48:38 +0530

    Update Server.js

--- Question1/Server.js
+++ Question1/Server.js
@@ -20,6 +20,9 @@ const apiEndpoints = {
   r: 'http://20.244.56.144/evaluation-service/rand',
 };
 
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ0NzAxMjUzLCJpYXQiOjE3NDQ3MDA5NTMsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjljZTI5OTBjLTc1MGItNGViNS04YzAwLTIzNTM1YzMyODlmMiIsInN1YiI6ImphaTQyNy5iZTIyQGNoaXRrYXJhLmVkdS5pbiJ9LCJlbWFpbCI6ImphaTQyNy5iZTIyQGNoaXRrYXJhLmVkdS5pbiIsIm5hbWUiOiJqYWkgY2hvcHJhIiwicm9sbE5vIjoiMjIxMDk5MDQyNyIsImFjY2Vzc0NvZGUiOiJQd3p1ZkciLCJjbGllbnRJRCI6IjljZTI5OTBjLTc1MGItNGViNS04YzAwLTIzNTM1YzMyODlmMiIsImNsaWVudFNlY3JldCI6ImhwaHhiTmV5dHV5UUhYbkUifQ.RtTylOFAqs2ZVQhxGOOPMtguqgsc5fpGDoPJeFpe-JQ'
+
+
 app.get('/numbers/:numberid', async (req, res) => {
   const { numberid } = req.params;
 
